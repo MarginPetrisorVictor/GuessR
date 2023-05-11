b7bb689ef24bc9987fe512236892a8f8c446d152
Crantz <Radasan.Gh.Teodor@student.utcluj.ro>
2023-05-11 20:24:43 +0300

    Database

---
+++ GuessR/Data/Migrations/20230511171442_Players.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuessR.Data.Migrations
+{
+    public partial class Players : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Player_AspNetUsers_UserId",
+                table: "Player");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GuessModel_UserId",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "SuccessPercentage",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPlayedGames",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "TotalScore",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "GuessModel");
+
+            migrationBuilder.RenameTable(
+                name: "Player",
+                newName: "Players");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Question",
+                table: "GuessModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "Players",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Players",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Players",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SuccessPercentage",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalPlayedGames",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalScore",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Players",
+                table: "Players",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_UserId",
+                table: "Players",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_AspNetUsers_UserId",
+                table: "Players",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_AspNetUsers_UserId",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Players",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_UserId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Question",
+                table: "GuessModel");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "SuccessPercentage",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPlayedGames",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TotalScore",
+                table: "Players");
+
+            migrationBuilder.RenameTable(
+                name: "Players",
+                newName: "Player");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "GuessModel",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "GuessModel",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "GuessModel",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "GuessModel",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SuccessPercentage",
+                table: "GuessModel",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalPlayedGames",
+                table: "GuessModel",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalScore",
+                table: "GuessModel",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "GuessModel",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GuessModel_UserId",
+                table: "GuessModel",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Player_AspNetUsers_UserId",
+                table: "Player",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
